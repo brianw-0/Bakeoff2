f6b29848d882a1b010000fcd94064be58a89b54a
Brian Wang <boan.wang@gmail.com>
2018-10-23 10:56:48 -0700

    Added click-through for the window, as well as restrictions for moving outside the window

--- js/index.js
+++ js/index.js
@@ -31,11 +31,14 @@ var customPan = new ZingTouch.Pan({
   threshold: 1
 });
 
+var windowWidth = $("#keyboard_window").width();
+var windowHeight = $("#keyboard_window").height();
+console.log("Width: " + windowWidth + " Height: " + windowHeight);
+
 zt.bind(windowElement, customPan, function(e) {
-    /*['currentDirection', Math.floor(e.detail.data[0].currentDirection) + "°"],
-    ['directionFromOrigin', Math.floor(e.detail.data[0].directionFromOrigin) + "°"],
-    ['distanceFromOrigin', Math.floor(e.detail.data[0].distanceFromOrigin) + "px"]*/
-  
+  console.log("swiped/pan on window");
+  $(".simple-keyboard").css({ "z-index": -1 });
+
   var distance = Math.floor(e.detail.data[0].distanceFromOrigin);
   var OriginDirection = Math.floor(e.detail.data[0].directionFromOrigin);
   var currentDirection = Math.floor(e.detail.data[0].currentDirection);
@@ -44,6 +47,19 @@ zt.bind(windowElement, customPan, function(e) {
   
   var x = e.detail.events[0].x // - windowElement.style.left;
   var y = e.detail.events[0].y // - windowElement.style.top;
+ 
+  
+  //console.log("x: " + (x-$("#keyboard_window").position().left - windowWidth) + " y: " + (y - $("#keyboard_window").position().top - windowHeight));
+  
+  //As an example, x-$("#keyboard_window").position().left) gives you the position of the cursor relative to inside the window
+  //So (x-$("#keyboard_window").position().left) > windowWidth is checking if the cursor has moved outside the right side of the window
+  //And (x-$("#keyboard_window").position().left) < 0 is checking if the cursor has moved outside the left side of the window
+  if((x-$("#keyboard_window").position().left) > windowWidth || (y - $("#keyboard_window").position().top) > windowHeight) {
+	  return;
+  }
+  if((x-$("#keyboard_window").position().left) < 0 || (y - $("#keyboard_window").position().top) < 0) {
+	  return;
+  }
   
   var distanceX = 0;
   var distanceY = 0;
@@ -61,8 +77,8 @@ zt.bind(windowElement, customPan, function(e) {
   
   //distanceX = Math.cos(currentDirection) * distance;
   //distanceY = Math.sin(currentDirection) * distance;
-  console.log("X: " + distanceX + " Y: " + distanceY);
-  console.log("C_X: " + currentDistanceX + " C_Y: " + currentDistanceY);
+  //console.log("X: " + distanceX + " Y: " + distanceY);
+  //console.log("C_X: " + currentDistanceX + " C_Y: " + currentDistanceY);
   
   //console.log("X: " + currentDistanceX + " Y: " + currentDistanceY);
   
@@ -74,5 +90,9 @@ zt.bind(windowElement, customPan, function(e) {
   currentDistanceX = $(".simple-keyboard").position().left;
   currentDistanceY = $(".simple-keyboard").position().top;
   
-  
+});
+
+zt.bind(windowElement, 'tap', function(e) {
+	console.log("Tapped on window");
+	$(".simple-keyboard").css({ "z-index": 1 });
 });
